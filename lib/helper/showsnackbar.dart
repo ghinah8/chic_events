4e9b6455dc4795a8e0f127a5a42cba3ab5410ecf
ghina habib <ghinahabib55@gmail.com>
2024-04-30 14:09:11 +0300

    all

---
+++ lib/helper/showsnackbar.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+void showsnackbar(BuildContext context, String message) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
+}
